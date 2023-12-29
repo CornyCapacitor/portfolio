fa5d78ceced90a1e79bded655d9b96b5bbe083e0
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-29 19:55:08 +0100

    cleaning some css and navbar logic + setup for the rest navbar link buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
+import { About } from './components/About'
+import { Contact } from './components/Contact'
+import { Experience } from './components/Experience'
 import { Home } from './components/Home'
 import { Navbar } from './components/Navbar'
+import { Projects } from './components/Projects'
 
 import './App.css'
+
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   )
