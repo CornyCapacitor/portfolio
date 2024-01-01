08606289ec5dd8b029a32e32deccf7f4b07c2de3
Mateusz Minder <mateusz.minder99@gmail.com>
2024-01-01 16:31:45 +0100

    filled experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -48,15 +48,15 @@ export const Experience = () => {
       <section>
         <h1>Main technologies</h1>
         <div className="languages">
-          <img src="html-icon.svg" id="html" className={selectedPrimary === "html" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
-          <img src="css-icon.svg" id="css" className={selectedPrimary === "css" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
-          <img src="js-icon.svg" id="js" className={selectedPrimary === "js" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
-          <img src="ts-icon.svg" id="ts" className={selectedPrimary === "ts" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
-          <img src="react-icon.svg" id="react" className={selectedPrimary === "react" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
+          <img src="html-icon.svg" id="HTML" className={selectedPrimary === "html" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
+          <img src="css-icon.svg" id="CSS" className={selectedPrimary === "css" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
+          <img src="js-icon.svg" id="JavaScript" className={selectedPrimary === "js" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
+          <img src="ts-icon.svg" id="TypeScript" className={selectedPrimary === "ts" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
+          <img src="react-icon.svg" id="React" className={selectedPrimary === "react" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "primary")} />
         </div>
         {selectedPrimary && (
-          <div>
-            <span>{selectedPrimary}</span>
+          <div className="description-container">
+            <span className="highlighted">{selectedPrimary}</span>
             <span>{primaryDescription}</span>
           </div>
         )}
@@ -64,15 +64,13 @@ export const Experience = () => {
       <section>
         <h1>Worth to mention</h1>
         <div className="additional-skills">
-          <img src="git-icon.svg" id="git" className={selectedSecondary === "git" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
-          <img src="github-icon.svg" id="github" className={selectedSecondary === "github" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
-          <img src="npm-icon.svg" id="npm" className={selectedSecondary === "npm" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
-          <img src="vite-icon.svg" id="vite" className={selectedSecondary === "vite" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
-          <img src="ai-icon.svg" id="ai" className={selectedSecondary === "ai" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
+          <img src="git-icon.svg" id="Git" className={selectedSecondary === "git" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
+          <img src="github-icon.svg" id="Github" className={selectedSecondary === "github" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
+          <img src="ai-icon.svg" id="AI" className={selectedSecondary === "ai" ? "selected" : ""} onClick={(e) => selectHandler((e.target as HTMLImageElement).id, "secondary")} />
         </div>
         {selectedSecondary && (
-          <div>
-            <span>{selectedSecondary}</span>
+          <div className="description-container">
+            <span className="highlighted">{selectedSecondary}</span>
             <span>{secondaryDescription}</span>
           </div>
         )}
@@ -80,7 +78,7 @@ export const Experience = () => {
       <section>
         <h1>Commercial experience</h1>
         <div className="commercial-experience">
-          <span>Almost 1 year of self-learning</span>
+          <span>While never being employed as a software developer, I've put my interest into many community-based activities, such as advent of code or was lurking through many open repositories to gain information about how big guys do their code. I've been learning magics of typescript for almost a year during my free time, and react for almost a half. Almost all of my knowledge comes from books and benevolence of the internet.</span>
         </div>
       </section>
     </div>
--- src/components/ExperienceList.ts
+++ src/components/ExperienceList.ts
@@ -1,42 +1,34 @@
 export const ExperienceList = [
   {
-    name: "html",
-    description: "html description",
+    name: "HTML",
+    description: "Although being aware that HTML isn't certainly a programming language, I want to mention that I'm also aware of proper writing html structure and signing certain elements, so it's suitable for different devices such as text-readers.",
   },
   {
-    name: "css",
-    description: "css description"
+    name: "CSS",
+    description: "I like to set my css clean, at least I think it is clean most of the time. I know how to create basic animations, how to manage same things using different properties and (I think so) have knowledge about few css tools such as styled components, scss, sass or tailwind."
   },
   {
-    name: "js",
-    description: "js description"
+    name: "JavaScript",
+    description: "I feel very comfortable writing JavaScript code. Never put much effort in learning other languages, but I must admit that JavaScript seems logical for me for most of the time. Surely I must be missing many useful techniques, but I'm willingful to learn those as fast as possible, to be even more efficient."
   },
   {
-    name: "ts",
-    description: "ts description"
+    name: "TypeScript",
+    description: "When starting to write JavaScript, it felt pretty satisfying due to it's easily learnable basics. When adviced to touch TypeScript, my perception of the code I write has changed. I started to notice many benefits of static typing, although I must admit, that sometimes TypeScript can get under my skin."
   },
   {
-    name: "react",
-    description: "react description"
+    name: "React",
+    description: "React is my first framework to which I have devoted most of my time. I am familiar with basic Hooks, custom Hooks and the nature of refreshing and working React code. I feel very comfortable in it and I would certainly like to continue writing commercial code in this framework."
   },
   {
-    name: "git",
-    description: "git description"
+    name: "Git",
+    description: "I'm familiar with the very basics of Git. I know how to prepare the code for pushes, know (At least I think so) how to write concise commit messages, how to manage branching and much more of these basic functionalities git provides."
   },
   {
-    name: "github",
-    description: "github description"
+    name: "Github",
+    description: "I did some github training to be prepared for participating in code reviewing, proper tracking of code version and similar functions. Know how to read it and participate without causing incomprehensible changes."
   },
   {
-    name: "npm",
-    description: "npm description"
-  },
-  {
-    name: "vite",
-    description: "vite description"
-  },
-  {
-    name: "ai",
-    description: "ai description"
+    name: "AI",
+    description: "I'm aware of the artificial intelligence existence and its rapid development over the last few years. I keep up to date with its development and the possibilities of using it during programming. I know that the solutions offered by the free variants are still full of bugs, but I am familiar with how to use their benefits to make tasks easier."
   },
 ]
