cc17faab177450ee3fb5e5858f0f47036fa415ea
Mateusz Minder <mateusz.minder99@gmail.com>
2024-01-03 20:05:09 +0100

    contact page setup

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,23 @@
+import './Contact.css'
+
 export const Contact = () => {
   return (
-    <div className="contact">Contact</div>
+    <div className="contact">
+      <h1>Contact me via:</h1>
+      <div className="contact-sources">
+        <a href="https://github.com/HornyCapacitor">
+          <img style={{ backgroundColor: "transparent" }} src="github-icon.svg" className="contact-icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/mateusz-minder-b19303257/">
+          <img src="linkedin-icon.svg" className="contact-icon" />
+        </a>
+        <a href="https://www.facebook.com/ElHombreFenomeno/">
+          <img src="facebook-icon.svg" className="contact-icon" />
+        </a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+          <img src="cv-file-icon.svg" className="contact-icon" />
+        </a>
+      </div>
+    </div>
   )
 }
