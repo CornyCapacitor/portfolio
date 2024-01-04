cc1de8139995e8ca3d8e49cf6d3a5b022c9c44c6
Mateusz Minder <mateusz.minder99@gmail.com>
2024-01-04 10:23:17 +0100

    responsiveness for home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,9 +7,10 @@ export const Home = () => (
   <div className="home">
     <span className="highlighted greet">Hi, my name is</span>
     <h1>Mateusz Minder.</h1>
-    <div className="typewriter">
+    <div className="typewriter-container">
       <h2>
         <SelfWritingText />
+        <span className="typewriter"></span>
       </h2>
     </div>
     <span className="description">I'm aspiring software engineer specializing in building web applications. All of my experience comes from web learning and coding my own things to challenge myself even further every next project I've made. Currently I'm focused on finding a suitable job to extend my skills and become one of the best front-end developers in future.</span>
